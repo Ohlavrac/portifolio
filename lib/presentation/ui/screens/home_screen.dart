db39c2793e3981268ca172c13201eeecaaadb836
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-01-09 10:56:34 -0300

    refactor: now use Visibility to hide windows

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -26,48 +26,54 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(
-            left: endpos.dx == 0 && endpos.dy == 0 ? width/2+100 : endpos.dx,
-            top: endpos.dx == 0 && endpos.dy == 0 ? height/2 : endpos.dy,
-            child: Draggable(
-              //feedbackOffset: endpos,
-              feedback: (Container(color: windowsOpen[0] == true ? Colors.orange : Colors.transparent, height: 100, width: 100,)),
-              child: (Container(color: windowsOpen[0] == true ? Colors.orange : Colors.transparent, height: 100, width: 100,)),
-              childWhenDragging: (Container(color: Colors.transparent, height: 100, width: 100)),
-              onDragUpdate: (details) {
-                setState(() {
-                  endpos = details.localPosition;
-                });
-              },
-              onDragEnd: (details) {
-                print(endpos);
-                endpos = details.offset;
-                setState(() {
-                  
-                });
-              },
+          Visibility(
+            visible: windowsOpen[0],
+            child: Positioned(
+              left: endpos.dx == 0 && endpos.dy == 0 ? width/2+100 : endpos.dx,
+              top: endpos.dx == 0 && endpos.dy == 0 ? height/2 : endpos.dy,
+              child: Draggable(
+                //feedbackOffset: endpos,
+                feedback: (Container(color: Colors.orange, height: 100, width: 100,)),
+                child: (Container(color: Colors.orange, height: 100, width: 100,)),
+                childWhenDragging: (Container(color: Colors.transparent, height: 100, width: 100)),
+                onDragUpdate: (details) {
+                  setState(() {
+                    endpos = details.localPosition;
+                  });
+                },
+                onDragEnd: (details) {
+                  print(endpos);
+                  endpos = details.offset;
+                  setState(() {
+                    
+                  });
+                },
+              ),
             ),
           ),
-      Positioned(
-        left: endpos2.dx == 0 && endpos2.dy == 0 ? width/2-50 : endpos2.dx,
-        top: endpos2.dx == 0 && endpos2.dy == 0 ? height/2-50 : endpos2.dy,
-        child: Draggable(
-          //feedbackOffset: endpos2,
-          feedback: (Container(color: windowsOpen[1] == true ? Colors.black : Colors.transparent, height: 100, width: 100,)),
-          child: (Container(color: windowsOpen[1] == true ? Colors.black : Colors.transparent, height: 100, width: 100,)),
-          childWhenDragging: (Container(color: Colors.transparent, height: 100, width: 100)),
-          onDragUpdate: (details) {
-            setState(() {
-              endpos2 = details.localPosition;
-            });
-          },
-          onDragEnd: (details) {
-            print(endpos2);
-            endpos2 = details.offset;
-            setState(() {
-              
-            });
-          },
+      Visibility(
+        visible: windowsOpen[1],
+        child: Positioned(
+          left: endpos2.dx == 0 && endpos2.dy == 0 ? width/2-50 : endpos2.dx,
+          top: endpos2.dx == 0 && endpos2.dy == 0 ? height/2-50 : endpos2.dy,
+          child: Draggable(
+            //feedbackOffset: endpos2,
+            feedback: (Container(color: Colors.black, height: 100, width: 100,)),
+            child: (Container(color: Colors.black, height: 100, width: 100,)),
+            childWhenDragging: (Container(color: Colors.transparent, height: 100, width: 100)),
+            onDragUpdate: (details) {
+              setState(() {
+                endpos2 = details.localPosition;
+              });
+            },
+            onDragEnd: (details) {
+              print(endpos2);
+              endpos2 = details.offset;
+              setState(() {
+                
+              });
+            },
+          ),
         ),
       ),
           Visibility(visible: true, child: Container(),)
