86022247897895c78c7d21973dbf5c3b0bad7b8c
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-01-09 10:02:06 -0300

    update

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -9,6 +9,12 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late Offset endpos = Offset.zero;
+  late Offset endpos2 = Offset.zero;
+
+  List<bool> windowsOpen = [
+    false,
+    false
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -18,29 +24,55 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: Stack(
         children: [
-          Center(child: Container(color: Colors.red, height: 100, width: 10,)), 
-          Positioned(
-            left: endpos.dx == 0 && endpos.dy == 0 ? width/2-50 : endpos.dx,
-            top: endpos.dx == 0 && endpos.dy == 0 ? height/2-50 : endpos.dy,
-            child: Draggable(
-              //feedbackOffset: endpos,
-              feedback: (Container(color: Colors.orange, height: 100, width: 100,)),
-              child: (Container(color: Colors.orange, height: 100, width: 100,)),
-              childWhenDragging: (Container(color: Colors.transparent, height: 100, width: 100)),
-              onDragUpdate: (details) {
-                setState(() {
-                  endpos = details.localPosition;
-                });
-              },
-              onDragEnd: (details) {
-                print(endpos);
-                endpos = details.offset;
-                setState(() {
-                  
-                });
-              },
-            ),
-          ),
+          Visibility(
+            visible: windowsOpen[0],
+            child: Positioned(
+              left: endpos.dx == 0 && endpos.dy == 0 ? width/2+100 : endpos.dx,
+              top: endpos.dx == 0 && endpos.dy == 0 ? height/2 : endpos.dy,
+              child: Draggable(
+                //feedbackOffset: endpos,
+                feedback: (Container(color: Colors.orange, height: 100, width: 100,)),
+                child: (Container(color: Colors.orange, height: 100, width: 100,)),
+                childWhenDragging: (Container(color: Colors.transparent, height: 100, width: 100)),
+                onDragUpdate: (details) {
+                  setState(() {
+                    endpos = details.localPosition;
+                  });
+                },
+                onDragEnd: (details) {
+                  print(endpos);
+                  endpos = details.offset;
+                  setState(() {
+                    
+                  });
+                },
+              ),
+            )),
+      Visibility(
+            visible: false,
+            child: Positioned(
+              left: endpos2.dx == 0 && endpos2.dy == 0 ? width/2-50 : endpos2.dx,
+              top: endpos2.dx == 0 && endpos2.dy == 0 ? height/2-50 : endpos2.dy,
+              child: Draggable(
+                //feedbackOffset: endpos2,
+                feedback: (Container(color: Colors.black, height: 100, width: 100,)),
+                child: (Container(color: Colors.black, height: 100, width: 100,)),
+                childWhenDragging: (Container(color: Colors.transparent, height: 100, width: 100)),
+                onDragUpdate: (details) {
+                  setState(() {
+                    endpos2 = details.localPosition;
+                  });
+                },
+                onDragEnd: (details) {
+                  print(endpos2);
+                  endpos2 = details.offset;
+                  setState(() {
+                    
+                  });
+                },
+              ),
+            )),
+          Visibility(visible: true, child: Container(),)
         ],
       ),
       bottomNavigationBar: BottomAppBar(
@@ -49,7 +81,13 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Row(
           children: [
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  windowsOpen[0] = true;
+                  endpos = Offset.zero;
+                });
+                print(windowsOpen);
+              },
               icon: Icon(Icons.computer), color: Colors.black,
               
             )
