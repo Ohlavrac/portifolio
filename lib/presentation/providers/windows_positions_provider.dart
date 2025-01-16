2605d39eb3baca5f46b72e511e59c59995cb53a5
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-01-16 09:38:28 -0300

    feat: add window positions provider

---
+++ lib/presentation/providers/windows_positions_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class WindowPositionProvider extends ChangeNotifier {
+  List<Offset> windowPositions = [
+    Offset.zero,
+    Offset.zero,
+    Offset.zero,
+    Offset.zero,
+  ];
+
+
+  void updateWindowPostion(int index, double dx, double dy) {
+    windowPositions[index] = Offset(dx, dy);
+    notifyListeners();
+  }
+
+  void resetWindowPosition(int index) {
+    windowPositions[index] = Offset.zero;
+    notifyListeners();
+  }
+}
