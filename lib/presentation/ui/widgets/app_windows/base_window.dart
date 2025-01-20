bd87e30a5c46b25c7bd103be6b9599e40b0a90b6
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-01-20 11:30:01 -0300

    refactor: add window model and windows provider

--- lib/presentation/ui/widgets/app_windows/base_window.dart
+++ lib/presentation/ui/widgets/app_windows/base_window.dart
@@ -1,25 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:portifolio/domain/models/window_model.dart';
 import 'package:portifolio/presentation/providers/open_windows_provider.dart';
 import 'package:portifolio/presentation/providers/screen_provider.dart';
 import 'package:portifolio/presentation/providers/windows_positions_provider.dart';
+import 'package:portifolio/presentation/providers/windows_provider.dart';
 import 'package:provider/provider.dart';
 
 import '../window_title_bar.dart';
 
 class BaseWindow extends StatefulWidget {
-  final int windowIndex;
-  final String title;
-  final double windowWidth;
-  final double windowHeight;
+  final WindowModel window;
 
   //final Widget content;
 
   const BaseWindow({
     super.key, 
-    required this.windowIndex, 
-    required this.title, 
-    required this.windowWidth, 
-    required this.windowHeight, 
+    required this.window 
   });
 
   @override
@@ -33,20 +29,21 @@ class _BaseWindowState extends State<BaseWindow> {
     var provider = Provider.of<OpenWindowsProvider>(context);
     var positionProvider = Provider.of<WindowPositionProvider>(context);
     var screenProvider = Provider.of<ScreenProvider>(context);
+    var windowsProvider = Provider.of<WindowsProvider>(context);
 
     return Visibility(
-      visible: provider.openWindows[widget.windowIndex],
+      visible: widget.window.isOpen,
       child: Positioned(
         
-        left: positionProvider.windowPositions[widget.windowIndex].dx == 0 && positionProvider.windowPositions[widget.windowIndex].dy == 0 ? screenProvider.screenWidth/2-50 : positionProvider.windowPositions[widget.windowIndex].dx,
-        top: positionProvider.windowPositions[widget.windowIndex].dx == 0 && positionProvider.windowPositions[widget.windowIndex].dy == 0 ? screenProvider.screenHeight/2-50 : positionProvider.windowPositions[widget.windowIndex].dy,
+        left: positionProvider.windowPositions[widget.window.windowID].dx == 0 && positionProvider.windowPositions[widget.window.windowID].dy == 0 ? screenProvider.screenWidth/2-50 : positionProvider.windowPositions[widget.window.windowID].dx,
+        top: positionProvider.windowPositions[widget.window.windowID].dx == 0 && positionProvider.windowPositions[widget.window.windowID].dy == 0 ? screenProvider.screenHeight/2-50 : positionProvider.windowPositions[widget.window.windowID].dy,
         
         child: Draggable(
           ignoringFeedbackPointer: false,
           feedbackOffset: Offset(100, 100),
           feedback: Container(
-            height: widget.windowHeight,
-            width: widget.windowWidth,
+            height: widget.window.windowHeight,
+            width: widget.window.windowWidth,
             color: Color(0xffc1c1c1),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -54,8 +51,8 @@ class _BaseWindowState extends State<BaseWindow> {
               children: [
                 WindowTitleBar(
                   titleBarHeight: 35, 
-                  titleBarWidth: widget.windowWidth-8, 
-                  title: widget.title, 
+                  titleBarWidth: widget.window.windowWidth-8, 
+                  title: widget.window.title, 
                   onPressedMinimize: () {
                     setState(() {
                     });
@@ -64,28 +61,29 @@ class _BaseWindowState extends State<BaseWindow> {
                   onPressedClose: () {}
                 ),
                 Container(
-                  height: widget.windowHeight-45,
-                  width: widget.windowWidth-5,
+                  height: widget.window.windowHeight-45,
+                  width: widget.window.windowWidth-5,
                   color: Color(0xffc1c1c1),
                   child: Container()
                 )
               ],
             ),
           ),
-          childWhenDragging: (Container(color: Colors.transparent, height: widget.windowHeight, width: widget.windowWidth,)),
+          childWhenDragging: (Container(color: Colors.transparent, height: widget.window.windowHeight, width: widget.window.windowWidth,)),
           onDragUpdate: (details) {
             setState(() {
-              positionProvider.updateWindowPostion(widget.windowIndex, details.localPosition.dx, details.localPosition.dy);
+              positionProvider.updateWindowPostion(widget.window.windowID, details.localPosition.dx, details.localPosition.dy);
             });
           },
           onDragEnd: (details) {
             setState(() {
-              positionProvider.updateWindowPostion(widget.windowIndex, details.offset.dx, details.offset.dy);
+              positionProvider.updateWindowPostion(widget.window.windowID, details.offset.dx, details.offset.dy);
+              windowsProvider.windowReposition(widget.window.windowID);
             });
           },
           child: Container(
-            height: widget.windowHeight,
-            width: widget.windowWidth,
+            height: widget.window.windowHeight,
+            width: widget.window.windowWidth,
             color: Color(0xffc1c1c1),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -93,25 +91,23 @@ class _BaseWindowState extends State<BaseWindow> {
               children: [
                 WindowTitleBar(
                   titleBarHeight: 35, 
-                  titleBarWidth: widget.windowWidth-8, 
-                  title: widget.title, 
+                  titleBarWidth: widget.window.windowWidth-8, 
+                  title: widget.window.title, 
                   onPressedMinimize: () {
-                    setState(() {
-                      provider.closeWindow(widget.windowIndex);
-                    });
+                    windowsProvider.closeWindow(widget.window.windowID);
                   },
                   onPressedMaximize: () {}, 
                   onPressedClose: () {
                     
                     setState(() {
-                      provider.closeWindow(widget.windowIndex);
-                      positionProvider.resetWindowPosition(widget.windowIndex);
+                      windowsProvider.closeWindow(widget.window.windowID);
+                      positionProvider.resetWindowPosition(widget.window.windowID);
                     });
                   }
                 ),
                 Container(
-                  height: widget.windowHeight-45,
-                  width: widget.windowWidth-5,
+                  height: widget.window.windowHeight-45,
+                  width: widget.window.windowWidth-5,
                   color: Color(0xffc1c1c1),
                   child: Container(),
                 )
