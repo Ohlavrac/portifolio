56e3e76e47b47392ffcf22e859bf24c0ee6c5c3d
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-01-22 12:43:10 -0300

    feat: add about me and education

--- lib/presentation/ui/widgets/app_windows/user_info.dart
+++ lib/presentation/ui/widgets/app_windows/user_info.dart
@@ -31,18 +31,25 @@ class UserInfo extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Image.asset("./assets/images/profile.png", scale: 2.5,),
+            SizedBox(width: 10,),
             Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.bold), child: Text("Computer Science - UESPI")),
-                DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.bold), child: Text("Age: 23 years"))
+                DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20), child: Text("Software Engining")),
+                SizedBox(height: 8,),
+                DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16), child: Text("Education:")),
+                DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.normal), child: Text("B.S.")),
+                DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.normal), child: Text("Computer Science")),
+                DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.normal), child: Text("Universidade Estadual\nDo Piaui - UESPI")),              
+                DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.normal), child: Text("Sep 2019 - Dec 2024")),
               ],
             )
           ],
         ),
+        SizedBox(height: 10,),
         DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.bold), child: Text("About Me:")),
-        Text("Hello, my name is João Victor Rodrigues Carvalho I'm a software enginer from Brazil."),
+        DefaultTextStyle(style: TextStyle(), child: Text(textAlign: TextAlign.justify, "Hi, my name is João Victor, I'm 23 years old and I'm a full-stack developer with a degree in Computer Science. I've always been fascinated by the world of technology, always trying to understand how the games I played worked, which was my main motivation to start programming. I currently work as a full-stack developer and I'm always looking for a new challenge or task to put my skills into practice and to learn something new.")),
         DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.bold), child: Text("Stacks:")),
         Text("FLUTTER")
       ],
