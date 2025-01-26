d7a6d5ab91b799f035f62623ea6269a1ad700ec7
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-01-25 22:12:22 -0300

    feat: add user info content

--- lib/presentation/ui/widgets/app_windows/user_info.dart
+++ lib/presentation/ui/widgets/app_windows/user_info.dart
@@ -36,7 +36,7 @@ class UserInfo extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20), child: Text("Software Engining")),
+                DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20), child: Text("Software Engineer")),
                 SizedBox(height: 8,),
                 DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16), child: Text("Education:")),
                 DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.normal), child: Text("B.S.")),
@@ -49,9 +49,25 @@ class UserInfo extends StatelessWidget {
         ),
         SizedBox(height: 10,),
         DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.bold), child: Text("About Me:")),
-        DefaultTextStyle(style: TextStyle(), child: Text(textAlign: TextAlign.justify, "Hi, my name is João Victor, I'm 23 years old and I'm a full-stack developer with a degree in Computer Science. I've always been fascinated by the world of technology, always trying to understand how the games I played worked, which was my main motivation to start programming. I currently work as a full-stack developer and I'm always looking for a new challenge or task to put my skills into practice and to learn something new.")),
-        DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.bold), child: Text("Stacks:")),
-        Text("FLUTTER")
+        Padding(
+          padding: const EdgeInsets.only(top: 2, bottom: 10),
+          child: DefaultTextStyle(style: TextStyle(), child: Text(textAlign: TextAlign.justify, "Hi, my name is João Victor, I'm 23 years old and I'm a full-stack developer with a degree in Computer Science. I've always been fascinated by the world of technology, always trying to understand how the games I played worked, which was my main motivation to start programming. I currently work as a full-stack developer and I'm always looking for a new challenge or task to put my skills into practice and to learn something new.")),
+        ),
+        DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.bold), child: Text("Programming Languages:")),
+        Padding(
+          padding: const EdgeInsets.only(top: 2, bottom: 10),
+          child: DefaultTextStyle(style: TextStyle(), child: Text("Dart | Java | Javascript | Python | C | Lua | Typescript"),),
+        ),
+        DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.bold), child: Text("Tecnologies:")),
+        Padding(
+          padding: const EdgeInsets.only(top: 2, bottom: 10),
+          child: DefaultTextStyle(style: TextStyle(), child: Text("Flutter | Spring Boot | Express | Firebase | Prisma | MySQL | MongoDB | PostgreSQL"),),
+        ),
+        DefaultTextStyle(style: TextStyle(fontWeight: FontWeight.bold), child: Text("Languages:")),
+        Padding(
+          padding: const EdgeInsets.only(top: 2, bottom: 10),
+          child: DefaultTextStyle(style: TextStyle(), child: Text("Portuguese (Fluent) | English (Advanced Intermediate)"),),
+        ),
       ],
     );
   }
