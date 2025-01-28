fbee9c9bc3d7a4d674cf97abc5735f5f1d77ce67
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-01-28 11:20:07 -0300

    feat: add icon model

---
+++ lib/domain/models/icon_model.dart
@@ -0,0 +1,8 @@
+class IconModel {
+  final int id;
+  final String imageUrl;
+  final double height;
+  final String title;
+
+  IconModel({required this.id, required this.imageUrl, required this.height, required this.title});
+}
