d49b9a9ed75491ef5a5edea9493ca22ca6946d11
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-01-30 12:34:25 -0300

    fix: desktop icon height

--- lib/domain/models/icon_model.dart
+++ lib/domain/models/icon_model.dart
@@ -2,7 +2,7 @@ class IconModel {
   final int id;
   final int windowId;
   final String imageUrl;
-  double height = 60;
+  double height = 55;
   final String title;
 
   IconModel({required this.windowId, required this.id, required this.imageUrl, required this.title});
