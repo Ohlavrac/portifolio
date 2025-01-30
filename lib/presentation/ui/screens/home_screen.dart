a0e2e5b41b6e7169987e6c5a734a5af5ee6bcec6
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-01-30 09:33:39 -0300

    feat: add icons on desktop

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -3,8 +3,11 @@ import 'package:portifolio/presentation/providers/screen_provider.dart';
 import 'package:portifolio/presentation/providers/windows_provider.dart';
 import 'package:portifolio/presentation/ui/widgets/app_windows/base_window.dart';
 import 'package:portifolio/presentation/ui/widgets/app_windows/user_info.dart';
+import 'package:portifolio/presentation/ui/widgets/desktop_icon.dart';
 import 'package:portifolio/presentation/ui/widgets/start_button.dart';
 import 'package:provider/provider.dart';
+
+import '../../../domain/models/icon_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -26,9 +29,23 @@ class _HomeScreenState extends State<HomeScreen> {
 
   List<Widget> iconsDesktop = [
     //Image.asset("./assets/icons/my-computer-icon.png", scale: 6.0,),
-    Image.asset("./assets/icons/my-computer-icon-2.png", scale: 4.0),
-    Image.asset("./assets/icons/my-network-icon.png", scale: 5.0),
-    Icon(Icons.browse_gallery)
+    Image.asset("./assets/icons/my-computer-icon.png", height: 60,),
+    Image.asset("./assets/icons/my-network-icon-2.png", height: 60,),
+    Image.asset("./assets/icons/my-network-icon-2.png", height: 60,),
+    Image.asset("./assets/icons/my-network-icon-2.png", height: 60,),
+    Image.asset("./assets/icons/my-network-icon-2.png", height: 60,),
+    Image.asset("./assets/icons/my-network-icon-2.png", height: 60,),
+    Image.asset("./assets/icons/my-network-icon-2.png", height: 60,),
+    Image.asset("./assets/icons/my-network-icon-2.png", height: 60,),
+
+  ];
+
+  List<IconModel> icons = [
+    IconModel(id: 1, windowId: 2, imageUrl: "./assets/icons/my-computer-icon-3.png", title: "my computer"),
+    IconModel(id: 2, windowId: 1, imageUrl: "./assets/icons/my-network-icon-2.png", title: "my network"),
+    IconModel(id: 3, windowId: 4, imageUrl: "./assets/icons/email-icon.png", title: "my email"),
+    IconModel(id: 4, windowId: 3, imageUrl: "./assets/icons/msn-icon.png", title: "MSN"),
+    IconModel(id: 5, windowId: 5, imageUrl: "./assets/icons/song-icon.png", title: "playlists"),
   ];
 
   @override
@@ -42,33 +59,35 @@ class _HomeScreenState extends State<HomeScreen> {
     screenProvider.screenHeight = height;
     screenProvider.screenWidth = width;
 
+    print("Altura: ${height} | icons*70 ${iconsDesktop.length * 100 + 70}");
+
     return Scaffold(
       backgroundColor: Color(0xFF4E6851),
       body: Stack(
         children: [
           GridView.builder(
-            itemCount: iconsDesktop.length,
-            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 100, crossAxisSpacing: 6, mainAxisSpacing: 5),
+            padding: EdgeInsets.all(10),
+            scrollDirection: Axis.vertical,
+            itemCount: icons.length,
+            //gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 100, crossAxisSpacing: 6, mainAxisSpacing: 5),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1, mainAxisExtent: 100),
             itemBuilder: (context, index) {
-              return ElevatedButton(
-                onPressed: () {
-              
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.all(1),
-                  backgroundColor: Colors.transparent,
-                  shadowColor: Colors.transparent,
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0))
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    iconsDesktop[index],
-                    Text("my-computer", style: TextStyle(color: Colors.black),)
-                  ],
-                ),
+              return Wrap(
+                direction: Axis.horizontal,
+                //verticalDirection: VerticalDirection.up,
+                children: [
+                  DesktopIcon(
+                    imageUrl: icons[index].imageUrl,
+                    height: icons[index].height,
+                    title: icons[index].title,
+                    onPressed: () {
+                      setState(() {
+                        windowsProvider.openWindow(icons[index].windowId);
+                        windowsProvider.windowReposition(icons[index].windowId);
+                      });
+                    },
+                  ),
+                ],
               );
             },
           ),
