e01a168254e075f8773443884541d025b47ec2ed
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-03-14 11:02:23 -0300

    feat: add new parans windowModel

--- lib/domain/models/window_model.dart
+++ lib/domain/models/window_model.dart
@@ -4,6 +4,9 @@ class WindowModel {
   late bool isOpen;
   final double windowWidth;
   final double windowHeight;
+  final bool hasMinimizeButton;
+  final bool hasMaximizeButton;
+  final bool hasCloseButton;
 
-  WindowModel({required this.windowID, required this.title, required this.isOpen, required this.windowWidth, required this.windowHeight});
+  WindowModel({required this.windowID, required this.title, required this.isOpen, required this.windowWidth, required this.windowHeight, required this.hasMinimizeButton, required this.hasMaximizeButton, required this.hasCloseButton});
 }
