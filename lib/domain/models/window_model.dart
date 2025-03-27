7ae027a07a5a83937bddc53de3e1b967cb5d8a31
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-03-27 06:39:34 -0300

    feat: add is minimized on window model

--- lib/domain/models/window_model.dart
+++ lib/domain/models/window_model.dart
@@ -2,11 +2,12 @@ class WindowModel {
   final int windowID;
   final String title;
   late bool isOpen;
+  late bool isMinimized;
   final double windowWidth;
   final double windowHeight;
   final bool hasMinimizeButton;
   final bool hasMaximizeButton;
   final bool hasCloseButton;
 
-  WindowModel({required this.windowID, required this.title, required this.isOpen, required this.windowWidth, required this.windowHeight, required this.hasMinimizeButton, required this.hasMaximizeButton, required this.hasCloseButton});
+  WindowModel({required this.windowID, required this.title, required this.isOpen, required this.isMinimized, required this.windowWidth, required this.windowHeight, required this.hasMinimizeButton, required this.hasMaximizeButton, required this.hasCloseButton});
 }
