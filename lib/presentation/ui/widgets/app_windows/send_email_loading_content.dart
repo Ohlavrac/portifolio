56ba012878607d7eaf709d81805b7dfad4c59bf6
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-04-11 11:59:22 -0300

    feat: add message, and loading (without func) on sending email

--- lib/presentation/ui/widgets/app_windows/send_email_loading_content.dart
+++ lib/presentation/ui/widgets/app_windows/send_email_loading_content.dart
@@ -11,14 +11,49 @@ class _SendEmailLoadingContentState extends State<SendEmailLoadingContent> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset("./assets/icons/email-icon.png"),
-            Text("Send Email. . .")
+            Image.asset("./assets/icons/email-icon.png", scale: 5,),
+            SizedBox(width: 20,),
+            DefaultTextStyle(
+              style: TextStyle(
+                fontWeight: FontWeight.bold
+              ),
+              child: Text("SENDING EMAIL . . .", style: TextStyle(fontFamily: "Retro2B", fontSize: 14),)
+            )
           ],
         ),
-
+        SizedBox(
+          height: 20,
+        ),
+        SizedBox(
+          height: 25,
+          width: 300,
+          child: Row(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 100,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 1),
+                      child: Container(
+                        width: 10,
+                        color: Colors.blue,
+                      ),
+                    );
+                  }
+                ),
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
